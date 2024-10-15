1689021ec00f0a2b344425beb771aa0591799801
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-15 11:12:08 -0600

    gawk -i inplace "{ gsub(/\/\/ AddRequiredPropertyCodePlaceholder/, \"if (string.IsNullOrWhiteSpace(castMemberAdminDto.Name2))\n\ \ \ \ \ \ \ \ {\n\ \ \ \ \ \ \ \ \ \ \ \ throw new Exception(\\\"Name 2 required.\\\");\n\ \ \ \ \ \ \ \ }\n\n\ \ \ \ \ \ \ \ // AddRequiredPropertyCodePlaceholder\") }; { print }" BlazorWebAppMovies.BusinessLogic/Repositories/Server/CastMemberAdminRepository.cs

--- BlazorWebAppMovies.BusinessLogic/Repositories/Server/CastMemberAdminRepository.cs
+++ BlazorWebAppMovies.BusinessLogic/Repositories/Server/CastMemberAdminRepository.cs
@@ -28,6 +28,11 @@ public class CastMemberAdminRepository(ApplicationDbContext applicationDbContext
             throw new Exception("Name 1 required.");
         }
 
+        if (string.IsNullOrWhiteSpace(castMemberAdminDto.Name2))
+        {
+            throw new Exception("Name 2 required.");
+        }
+
         // AddRequiredPropertyCodePlaceholder
         // if (string.IsNullOrWhiteSpace(castMemberAdminDto.Title))
         // {
