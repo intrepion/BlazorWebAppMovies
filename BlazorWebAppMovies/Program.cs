68c676d85b314850fc6f2a54de8cd0cf244fda47
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-15 11:11:05 -0600

    gawk -i inplace "{ gsub(/\/\/ RegisterServerServiceCodePlaceholder/, \"builder.Services.AddScoped<ICastMemberAdminRepository, CastMemberAdminRepository>();\n// RegisterServerServiceCodePlaceholder\") }; { print }" BlazorWebAppMovies/Program.cs

--- BlazorWebAppMovies/Program.cs
+++ BlazorWebAppMovies/Program.cs
@@ -61,6 +61,7 @@ builder.Services.AddScoped(http => new HttpClient
 builder.Services.AddScoped<IApplicationRoleAdminRepository, ApplicationRoleAdminRepository>();
 builder.Services.AddScoped<IApplicationUserAdminRepository, ApplicationUserAdminRepository>();
 
+builder.Services.AddScoped<ICastMemberAdminRepository, CastMemberAdminRepository>();
 // RegisterServerServiceCodePlaceholder
 
 var app = builder.Build();
