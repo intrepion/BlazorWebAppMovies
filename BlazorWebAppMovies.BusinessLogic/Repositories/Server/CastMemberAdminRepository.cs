2aa859c99bef8be64a8669892d57fbc105cfa61d
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-15 11:11:41 -0600

    gawk -i inplace "{ gsub(/\/\/ IncludeTableCodePlaceholder/, \".Include(x => x.CastMemberMovies)\n\ \ \ \ // IncludeTableCodePlaceholder\") }; { print }" BlazorWebAppMovies.BusinessLogic/Repositories/Server/CastMemberAdminRepository.cs

--- BlazorWebAppMovies.BusinessLogic/Repositories/Server/CastMemberAdminRepository.cs
+++ BlazorWebAppMovies.BusinessLogic/Repositories/Server/CastMemberAdminRepository.cs
@@ -126,7 +126,8 @@ public class CastMemberAdminRepository(ApplicationDbContext applicationDbContext
 
         return await _applicationDbContext.CastMembers
 
-            // IncludeTableCodePlaceholder
+            .Include(x => x.CastMemberMovies)
+    // IncludeTableCodePlaceholder
 
             .ToListAsync();
     }
