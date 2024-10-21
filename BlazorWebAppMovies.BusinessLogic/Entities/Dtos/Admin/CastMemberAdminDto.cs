c62d876308fbba273f46579e09a6aa6651bdb946
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-21 03:43:14 -0600

    gawk -i inplace "{ gsub(/\/\/ DtoToEntityPropertyPlaceholder/, \"Name2 = castMemberAdminDto.Name2,\n\ \ \ \ \ \ \ \ \ \ \ \ // DtoToEntityPropertyPlaceholder\") }; { print }" BlazorWebAppMovies.BusinessLogic/Entities/Dtos/Admin/CastMemberAdminDto.cs

--- BlazorWebAppMovies.BusinessLogic/Entities/Dtos/Admin/CastMemberAdminDto.cs
+++ BlazorWebAppMovies.BusinessLogic/Entities/Dtos/Admin/CastMemberAdminDto.cs
@@ -34,6 +34,7 @@ public class CastMemberAdminDto
             Id = castMemberAdminDto?.Id ?? new Guid(),
 
             Name1 = castMemberAdminDto.Name1,
+            Name2 = castMemberAdminDto.Name2,
             // DtoToEntityPropertyPlaceholder
         };
     }
