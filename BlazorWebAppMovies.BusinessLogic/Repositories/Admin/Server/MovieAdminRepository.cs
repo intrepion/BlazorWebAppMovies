b2b27bcfa9aea94384c070927fadfb01cd3d2434
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-21 03:45:22 -0600

    gawk -i inplace "{ gsub(/\/\/ EditRequiredPropertyCodePlaceholder/, \"if (string.IsNullOrWhiteSpace(movieAdminDto.Title))\n\ \ \ \ \ \ \ \ {\n\ \ \ \ \ \ \ \ \ \ \ \ throw new Exception(\\\"Title required.\\\");\n\ \ \ \ \ \ \ \ }\n\n\ \ \ \ \ \ \ \ // EditRequiredPropertyCodePlaceholder\") }; { print }" BlazorWebAppMovies.BusinessLogic/Repositories/Admin/Server/MovieAdminRepository.cs

--- BlazorWebAppMovies.BusinessLogic/Repositories/Admin/Server/MovieAdminRepository.cs
+++ BlazorWebAppMovies.BusinessLogic/Repositories/Admin/Server/MovieAdminRepository.cs
@@ -93,6 +93,11 @@ public class MovieAdminRepository(ApplicationDbContext applicationDbContext) : I
             throw new Exception("HumanNamePlaceholder not found.");
         }
 
+        if (string.IsNullOrWhiteSpace(movieAdminDto.Title))
+        {
+            throw new Exception("Title required.");
+        }
+
         // EditRequiredPropertyCodePlaceholder
 
         databaseMovie.ApplicationUserUpdatedBy = user;
