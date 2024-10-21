91aae0711750cee5be81b77e478e7f226788c528
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-21 03:42:32 -0600

    gawk -i inplace "{ gsub(/EntityNamePlaceholder/, \"CastMember\") }; { print }" BlazorWebAppMovies.BusinessLogic/Repositories/Admin/ICastMemberAdminRepository.cs

--- BlazorWebAppMovies.BusinessLogic/Repositories/Admin/ICastMemberAdminRepository.cs
+++ BlazorWebAppMovies.BusinessLogic/Repositories/Admin/ICastMemberAdminRepository.cs
@@ -2,11 +2,11 @@
 
 namespace ApplicationNamePlaceholder.BusinessLogic.Repositories.Admin;
 
-public interface IEntityNamePlaceholderAdminRepository
+public interface ICastMemberAdminRepository
 {
-    Task<EntityNamePlaceholderAdminDto?> AddAsync(EntityNamePlaceholderAdminDto castMember);
+    Task<CastMemberAdminDto?> AddAsync(CastMemberAdminDto castMember);
     Task<bool> DeleteAsync(string userName, Guid id);
-    Task<EntityNamePlaceholderAdminDto?> EditAsync(EntityNamePlaceholderAdminDto castMember);
-    Task<List<EntityNamePlaceholderAdminDto>?> GetAllAsync(string userName);
-    Task<EntityNamePlaceholderAdminDto?> GetByIdAsync(string userName, Guid id);
+    Task<CastMemberAdminDto?> EditAsync(CastMemberAdminDto castMember);
+    Task<List<CastMemberAdminDto>?> GetAllAsync(string userName);
+    Task<CastMemberAdminDto?> GetByIdAsync(string userName, Guid id);
 }
