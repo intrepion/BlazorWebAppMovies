8550589d3d812911e426edd487f20f44411b1f0d
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-22 02:20:16 -0600

    gawk -i inplace "{ gsub(/EntityLowercaseNamePlaceholder/, \"castMemberMovie\") }; { print }" BlazorWebAppMovies.BusinessLogic/Entities/Dtos/Admin/CastMemberMovieAdminDto.cs

--- BlazorWebAppMovies.BusinessLogic/Entities/Dtos/Admin/CastMemberMovieAdminDto.cs
+++ BlazorWebAppMovies.BusinessLogic/Entities/Dtos/Admin/CastMemberMovieAdminDto.cs
@@ -7,27 +7,27 @@ public class EntityNamePlaceholderAdminDto
 
     // DtoPropertyPlaceholder
 
-    public static EntityNamePlaceholderAdminDto FromEntityNamePlaceholder(EntityNamePlaceholder? EntityLowercaseNamePlaceholder)
+    public static EntityNamePlaceholderAdminDto FromEntityNamePlaceholder(EntityNamePlaceholder? castMemberMovie)
     {
-        if (EntityLowercaseNamePlaceholder == null)
+        if (castMemberMovie == null)
         {
             return new EntityNamePlaceholderAdminDto();
         }
 
         return new EntityNamePlaceholderAdminDto
         {
-            Id = EntityLowercaseNamePlaceholder.Id,
+            Id = castMemberMovie.Id,
 
             // EntityToDtoPlaceholder
         };
     }
 
-    public static EntityNamePlaceholder ToEntityNamePlaceholder(ApplicationUser? applicationUser, EntityNamePlaceholderAdminDto? EntityLowercaseNamePlaceholderAdminDto)
+    public static EntityNamePlaceholder ToEntityNamePlaceholder(ApplicationUser? applicationUser, EntityNamePlaceholderAdminDto? castMemberMovieAdminDto)
     {
         return new EntityNamePlaceholder
         {
             ApplicationUserUpdatedBy = applicationUser ?? new ApplicationUser(),
-            Id = EntityLowercaseNamePlaceholderAdminDto?.Id ?? new Guid(),
+            Id = castMemberMovieAdminDto?.Id ?? new Guid(),
 
             // DtoToEntityPropertyPlaceholder
         };
