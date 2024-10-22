26824a528c15b525190e6067cc92f1435ac6aff7
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-22 02:21:53 -0600

    gawk -i inplace "{ gsub(/\/\/ EditDatabasePropertyCodePlaceholder/, \"databaseMovie.Title = movieAdminDto?.Title ?? string.Empty;\n\ \ \ \ \ \ \ \ // EditDatabasePropertyCodePlaceholder\") }; { print }" BlazorWebAppMovies.BusinessLogic/Repositories/Admin/Server/MovieAdminRepository.cs

--- BlazorWebAppMovies.BusinessLogic/Repositories/Admin/Server/MovieAdminRepository.cs
+++ BlazorWebAppMovies.BusinessLogic/Repositories/Admin/Server/MovieAdminRepository.cs
@@ -91,6 +91,7 @@ public class MovieAdminRepository(ApplicationDbContext applicationDbContext) : I
 
         databaseMovie.ApplicationUserUpdatedBy = user;
 
+        databaseMovie.Title = movieAdminDto?.Title ?? string.Empty;
         // EditDatabasePropertyCodePlaceholder
 
         await _applicationDbContext.SaveChangesAsync();
