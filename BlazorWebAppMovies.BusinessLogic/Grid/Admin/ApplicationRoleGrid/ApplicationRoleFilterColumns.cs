c9836b4e1aadb63177bfff39854c0c79f7a9ba20
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-04 00:21:42 -0700

    gawk -i inplace "{ gsub(/ApplicationNamePlaceholder/, \"BlazorWebAppMovies\") }; { print }" BlazorWebAppMovies.BusinessLogic/Grid/Admin/ApplicationRoleGrid/*

--- BlazorWebAppMovies.BusinessLogic/Grid/Admin/ApplicationRoleGrid/ApplicationRoleFilterColumns.cs
+++ BlazorWebAppMovies.BusinessLogic/Grid/Admin/ApplicationRoleGrid/ApplicationRoleFilterColumns.cs
@@ -1,4 +1,4 @@
-﻿namespace ApplicationNamePlaceholder.BusinessLogic.Grid.Admin.ApplicationRoleGrid;
+﻿namespace BlazorWebAppMovies.BusinessLogic.Grid.Admin.ApplicationRoleGrid;
 
 // Sort options.
 public enum ApplicationRoleFilterColumns
