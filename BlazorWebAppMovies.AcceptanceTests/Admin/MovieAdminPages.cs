b206079af6cb26c07794a07aafb318d1cffb146b
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-04 00:20:50 -0700

    gawk -i inplace "{ gsub(/PluralNamePlaceholder/, \"Movies\") }; { print }" BlazorWebAppMovies.AcceptanceTests/Admin/MovieAdminPages.cs

--- BlazorWebAppMovies.AcceptanceTests/Admin/MovieAdminPages.cs
+++ BlazorWebAppMovies.AcceptanceTests/Admin/MovieAdminPages.cs
@@ -14,7 +14,7 @@ public partial class MovieAdminPages : PageTest
         var faker = new Faker();
         string aRandomString = faker.Random.String2(10);
         string someRandomString = faker.Random.String2(10);
-        await Page.GetByRole(AriaRole.Link, new() { Name = "PluralNamePlaceholder" }).ClickAsync();
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Movies" }).ClickAsync();
         await Expect(Page).ToHaveTitleAsync("Movie Home");
         await Page.GetByRole(AriaRole.Link, new() { Name = "Create New" }).ClickAsync();
         await Expect(Page).ToHaveTitleAsync("Movie Add");
