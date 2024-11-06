d12b0abd74af4e74c963bcd428fbc040e1e31f3b
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-06 14:18:15 -0700

    gawk -i inplace "{ gsub(/LowercaseNamePlaceholder/, \"castMember\") }; { print }" BlazorWebAppMovies.AcceptanceTests/Admin/CastMemberAdminPages.cs

--- BlazorWebAppMovies.AcceptanceTests/Admin/CastMemberAdminPages.cs
+++ BlazorWebAppMovies.AcceptanceTests/Admin/CastMemberAdminPages.cs
@@ -14,7 +14,7 @@ public partial class CastMemberAdminPages : PageTest
         var faker = new Faker();
         var aRandomString = faker.Random.String2(10);
         var someRandomString = faker.Random.String2(10);
-        await Page.GetByTestId("LowercaseNamePlaceholderNavLink").ClickAsync();
+        await Page.GetByTestId("castMemberNavLink").ClickAsync();
         await Expect(Page).ToHaveTitleAsync("Cast Member Home");
         await Page.GetByRole(AriaRole.Link, new() { Name = "Create New" }).ClickAsync();
         await Expect(Page).ToHaveTitleAsync("Cast Member Add");
