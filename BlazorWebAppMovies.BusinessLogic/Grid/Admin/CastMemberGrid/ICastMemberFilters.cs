76477b21367c93bea08abad31cf9b96c3e36fdd7
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-06 14:18:11 -0700

    gawk -i inplace "{ gsub(/EntityNamePlaceholder/, \"CastMember\") }; { print }" BlazorWebAppMovies.BusinessLogic/Grid/Admin/CastMemberGrid/ICastMemberFilters.cs

--- BlazorWebAppMovies.BusinessLogic/Grid/Admin/CastMemberGrid/ICastMemberFilters.cs
+++ BlazorWebAppMovies.BusinessLogic/Grid/Admin/CastMemberGrid/ICastMemberFilters.cs
@@ -1,10 +1,10 @@
-﻿namespace BlazorWebAppMovies.BusinessLogic.Grid.Admin.EntityNamePlaceholderGrid;
+﻿namespace BlazorWebAppMovies.BusinessLogic.Grid.Admin.CastMemberGrid;
 
 // Interface for filtering.
-public interface IEntityNamePlaceholderFilters
+public interface ICastMemberFilters
 {
-    // The EntityNamePlaceholderFilterColumns being filtered on.
-    EntityNamePlaceholderFilterColumns FilterColumn { get; set; }
+    // The CastMemberFilterColumns being filtered on.
+    CastMemberFilterColumns FilterColumn { get; set; }
 
     // Loading indicator.
     bool Loading { get; set; }
@@ -18,6 +18,6 @@ public interface IEntityNamePlaceholderFilters
     // Gets or sets a value indicating if the sort is ascending or descending.
     bool SortAscending { get; set; }
 
-    // The EntityNamePlaceholderFilterColumns being sorted.
-    EntityNamePlaceholderFilterColumns SortColumn { get; set; }
+    // The CastMemberFilterColumns being sorted.
+    CastMemberFilterColumns SortColumn { get; set; }
 }
