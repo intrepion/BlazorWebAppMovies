0e6882a122cc2c18065ba5078476a365dea755ad
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-08 14:03:03 -0700

    gawk -i inplace "{ gsub(/ApplicationNamePlaceholder/, \"BlazorWebAppMovies\") }; { print }" BlazorWebAppMovies.BusinessLogic/Grid/Admin/ApplicationRoleGrid/*

--- BlazorWebAppMovies.BusinessLogic/Grid/Admin/ApplicationRoleGrid/ApplicationRoleGridControls.cs
+++ BlazorWebAppMovies.BusinessLogic/Grid/Admin/ApplicationRoleGrid/ApplicationRoleGridControls.cs
@@ -1,4 +1,4 @@
-namespace ApplicationNamePlaceholder.BusinessLogic.Grid.Admin.ApplicationRoleGrid;
+namespace BlazorWebAppMovies.BusinessLogic.Grid.Admin.ApplicationRoleGrid;
 
 // State of grid filters.
 public class ApplicationRoleGridControls(IPageHelper pageHelper) : IApplicationRoleFilters
