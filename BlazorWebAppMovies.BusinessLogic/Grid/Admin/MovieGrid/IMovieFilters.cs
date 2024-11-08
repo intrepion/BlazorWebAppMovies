2491368107bb20943375cf8ca9b7a8c9a516219c
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-08 14:02:24 -0700

    gawk -i inplace "{ gsub(/EntityNamePlaceholder/, \"Movie\") }; { print }" BlazorWebAppMovies.BusinessLogic/Grid/Admin/MovieGrid/IMovieFilters.cs

--- BlazorWebAppMovies.BusinessLogic/Grid/Admin/MovieGrid/IMovieFilters.cs
+++ BlazorWebAppMovies.BusinessLogic/Grid/Admin/MovieGrid/IMovieFilters.cs
@@ -1,10 +1,10 @@
-﻿namespace BlazorWebAppMovies.BusinessLogic.Grid.Admin.EntityNamePlaceholderGrid;
+﻿namespace BlazorWebAppMovies.BusinessLogic.Grid.Admin.MovieGrid;
 
 // Interface for filtering.
-public interface IEntityNamePlaceholderFilters
+public interface IMovieFilters
 {
-    // The EntityNamePlaceholderFilterColumns being filtered on.
-    EntityNamePlaceholderFilterColumns FilterColumn { get; set; }
+    // The MovieFilterColumns being filtered on.
+    MovieFilterColumns FilterColumn { get; set; }
 
     // Loading indicator.
     bool Loading { get; set; }
@@ -18,6 +18,6 @@ public interface IEntityNamePlaceholderFilters
     // Gets or sets a value indicating if the sort is ascending or descending.
     bool SortAscending { get; set; }
 
-    // The EntityNamePlaceholderFilterColumns being sorted.
-    EntityNamePlaceholderFilterColumns SortColumn { get; set; }
+    // The MovieFilterColumns being sorted.
+    MovieFilterColumns SortColumn { get; set; }
 }
