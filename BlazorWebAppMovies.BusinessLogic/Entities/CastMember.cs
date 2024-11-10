d359645b33c350b237075dc1f994036640b87ff0
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-09 20:34:17 -0700

    gawk -i inplace "{ gsub(/ApplicationNamePlaceholder/, \"BlazorWebAppMovies\") }; { print }" BlazorWebAppMovies.BusinessLogic/Entities/*

--- BlazorWebAppMovies.BusinessLogic/Entities/CastMember.cs
+++ BlazorWebAppMovies.BusinessLogic/Entities/CastMember.cs
@@ -1,6 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace ApplicationNamePlaceholder.BusinessLogic.Entities;
+namespace BlazorWebAppMovies.BusinessLogic.Entities;
 
 public class CastMember
 {
