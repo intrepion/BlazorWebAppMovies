d5c91bacf264f7f81e7ed06f2498df4354c3872f
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-14 01:39:09 -0700

    cp ../intrepion-bash/languages/csharp/dotnet/files/entity/ApplicationNamePlaceholder.BusinessLogic/Entities/Maps/EntityNamePlaceholderMap.cs BlazorWebAppMovies.BusinessLogic/Entities/Maps/CastMemberMovieMap.cs

---
+++ BlazorWebAppMovies.BusinessLogic/Entities/Maps/CastMemberMovieMap.cs
@@ -0,0 +1,18 @@
+﻿using CsvHelper.Configuration;
+
+namespace ApplicationNamePlaceholder.BusinessLogic.Entities.Maps;
+
+public sealed class EntityNamePlaceholderMap : ClassMap<EntityNamePlaceholder>
+{
+    public EntityNamePlaceholderMap()
+    {
+        Map(m => m.ApplicationUserUpdatedBy).Ignore();
+        Map(m => m.Id).Ignore();
+
+        // MappingCodePlaceholder
+        // Map(m => m.Name).Name("Name");
+        // Map(m => m.NormalizedName)
+        //     .Name("Name")
+        //     .Convert(args => args.Row.GetField("Name")?.ToUpperInvariant());
+    }
+}
