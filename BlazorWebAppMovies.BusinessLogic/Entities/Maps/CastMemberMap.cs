f3d33f036dcbc60512513ff28bc7e62d7145baca
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-14 01:38:35 -0700

    gawk -i inplace "{ gsub(/\/\/ MappingCodePlaceholder/, \"Map(m => m.NormalizedName2).Name(\\\"Name2\\\").Convert(args => args.Row.GetField(\\\"Name2\\\")?.ToUpperInvariant());\n\ \ \ \ \ \ \ \ // MappingCodePlaceholder\") }; { print }" BlazorWebAppMovies.BusinessLogic/Entities/Maps/CastMemberMap.cs

--- BlazorWebAppMovies.BusinessLogic/Entities/Maps/CastMemberMap.cs
+++ BlazorWebAppMovies.BusinessLogic/Entities/Maps/CastMemberMap.cs
@@ -12,6 +12,7 @@ public sealed class CastMemberMap : ClassMap<CastMember>
         Map(m => m.Name1).Name("Name1");
         Map(m => m.NormalizedName1).Name("Name1").Convert(args => args.Row.GetField("Name1")?.ToUpperInvariant());
         Map(m => m.Name2).Name("Name2");
+        Map(m => m.NormalizedName2).Name("Name2").Convert(args => args.Row.GetField("Name2")?.ToUpperInvariant());
         // MappingCodePlaceholder
         // Map(m => m.Name).Name("Name");
         // Map(m => m.NormalizedName)
