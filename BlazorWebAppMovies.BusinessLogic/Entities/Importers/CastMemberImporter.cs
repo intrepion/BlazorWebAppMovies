7201cc878575d6fca415c039612ccd5fab4999f8
Oliver Forral <intrepion@gmail.com>
2024-11-18 21:12:40 -0700

    gawk -i inplace "{ gsub(/\/\/ CompositeKeyCodePlaceholder/, \"\\\\&\\\\& x.NormalizedName1 == castMember.NormalizedName1\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ // CompositeKeyCodePlaceholder\") }; { print }" BlazorWebAppMovies.BusinessLogic/Entities/Importers/CastMemberImporter.cs

--- BlazorWebAppMovies.BusinessLogic/Entities/Importers/CastMemberImporter.cs
+++ BlazorWebAppMovies.BusinessLogic/Entities/Importers/CastMemberImporter.cs
@@ -58,6 +58,7 @@ public static class CastMemberImporter
 
             var dbCastMember = await context.CastMembers.SingleOrDefaultAsync(
                 x => true
+                && x.NormalizedName1 == castMember.NormalizedName1
                 // CompositeKeyCodePlaceholder
             );
 
