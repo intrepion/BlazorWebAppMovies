3ff870f540feeb4c557a440afb5c20b5c5a2d579
Oliver Forral <intrepion@gmail.com>
2024-11-18 21:13:03 -0700

    gawk -i inplace "{ gsub(/\/\/ SortExpressionCodePlaceholder/, \"{ CastMemberFilterColumns.Name1, x => x != null \\\\&\\\\& x.Name1 != null ? x.Name1 : string.Empty },\n\ \ \ \ \ \ \ \ \ \ \ \ // SortExpressionCodePlaceholder\") }; { print }" BlazorWebAppMovies.BusinessLogic/Grid/Admin/CastMemberGrid/CastMemberGridQueryAdapter.cs

--- BlazorWebAppMovies.BusinessLogic/Grid/Admin/CastMemberGrid/CastMemberGridQueryAdapter.cs
+++ BlazorWebAppMovies.BusinessLogic/Grid/Admin/CastMemberGrid/CastMemberGridQueryAdapter.cs
@@ -14,6 +14,7 @@ public class CastMemberGridQueryAdapter
         {
             { CastMemberFilterColumns.Id, x => !x.Id.Equals(Guid.Empty) ? x.Id.ToString() : string.Empty },
 
+            { CastMemberFilterColumns.Name1, x => x != null && x.Name1 != null ? x.Name1 : string.Empty },
             // SortExpressionCodePlaceholder
         };
 
