83d66d4dc5692e46baca312cbeee3c66a7646169
Oliver Forral <intrepion@gmail.com>
2024-11-18 21:16:54 -0700

    gawk -i inplace "{ gsub(/\/\/ SortExpressionCodePlaceholder/, \"{ MovieFilterColumns.Title, x => x != null \\\\&\\\\& x.Title != null ? x.Title : string.Empty },\n\ \ \ \ \ \ \ \ \ \ \ \ // SortExpressionCodePlaceholder\") }; { print }" BlazorWebAppMovies.BusinessLogic/Grid/Admin/MovieGrid/MovieGridQueryAdapter.cs

--- BlazorWebAppMovies.BusinessLogic/Grid/Admin/MovieGrid/MovieGridQueryAdapter.cs
+++ BlazorWebAppMovies.BusinessLogic/Grid/Admin/MovieGrid/MovieGridQueryAdapter.cs
@@ -14,6 +14,7 @@ public class MovieGridQueryAdapter
         {
             { MovieFilterColumns.Id, x => !x.Id.Equals(Guid.Empty) ? x.Id.ToString() : string.Empty },
 
+            { MovieFilterColumns.Title, x => x != null && x.Title != null ? x.Title : string.Empty },
             // SortExpressionCodePlaceholder
         };
 
