bf76d79b80db0a677a64e1b752e509aee5f434bd
Oliver Forral <intrepion@gmail.com>
2024-11-18 21:14:53 -0700

    gawk -i inplace "{ gsub(/\/\/ RecordPropertyCodePlaceholder/, \"public string CastMember_NormalizedName2 { get; set; } = string.Empty;\n\ \ \ \ // RecordPropertyCodePlaceholder\") }; { print }" BlazorWebAppMovies.BusinessLogic/Entities/Records/CastMemberMovieRecord.cs

--- BlazorWebAppMovies.BusinessLogic/Entities/Records/CastMemberMovieRecord.cs
+++ BlazorWebAppMovies.BusinessLogic/Entities/Records/CastMemberMovieRecord.cs
@@ -3,5 +3,6 @@
 public class CastMemberMovieRecord
 {
     public string CastMember_NormalizedName1 { get; set; } = string.Empty;
+    public string CastMember_NormalizedName2 { get; set; } = string.Empty;
     // RecordPropertyCodePlaceholder
 }
