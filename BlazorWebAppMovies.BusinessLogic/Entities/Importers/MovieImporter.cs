8908a9fe9993ac979dcad7933f7ba9f22af473ee
Oliver Forral <intrepion@gmail.com>
2024-11-18 21:16:51 -0700

    gawk -i inplace "{ gsub(/\/\/ NewEntityCodePlaceholder/, \"Title = record.Title,\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ // NewEntityCodePlaceholder\") }; { print }" BlazorWebAppMovies.BusinessLogic/Entities/Importers/MovieImporter.cs

--- BlazorWebAppMovies.BusinessLogic/Entities/Importers/MovieImporter.cs
+++ BlazorWebAppMovies.BusinessLogic/Entities/Importers/MovieImporter.cs
@@ -53,6 +53,7 @@ public static class MovieImporter
             {
                 ApplicationUserUpdatedBy = applicationUserUpdatedBy,
 
+                Title = record.Title,
                 // NewEntityCodePlaceholder
             };
 
