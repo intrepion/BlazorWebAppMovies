2f3a1e73e274b54e83d1ec147beffdeecb4084a9
Oliver Forral <intrepion@gmail.com>
2024-11-18 21:17:10 -0700

    gawk -i inplace "{ gsub(/\/\/ ActualPropertyPlaceholder/, \"public string Year { get; set; } = string.Empty;\n\ \ \ \ // ActualPropertyPlaceholder\") }; { print }" BlazorWebAppMovies.BusinessLogic/Entities/Movie.cs

--- BlazorWebAppMovies.BusinessLogic/Entities/Movie.cs
+++ BlazorWebAppMovies.BusinessLogic/Entities/Movie.cs
@@ -13,5 +13,6 @@ public class Movie
     [Required]
     public string NormalizedTitle { get; set; } = string.Empty;
     [Required]
+    public string Year { get; set; } = string.Empty;
     // ActualPropertyPlaceholder
 }
