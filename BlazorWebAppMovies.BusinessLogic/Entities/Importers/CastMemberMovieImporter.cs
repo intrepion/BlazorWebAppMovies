4b4829e329caedde366630502b3686a42ff48469
Oliver Forral <intrepion@gmail.com>
2024-11-20 15:42:37 -0700

    gawk -i inplace "{ gsub(/\/\/ CompositeKeyCodePlaceholder/, \"\\\\&\\\\& x.CastMember == castMember\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ // CompositeKeyCodePlaceholder\") }; { print }" BlazorWebAppMovies.BusinessLogic/Entities/Importers/CastMemberMovieImporter.cs

--- BlazorWebAppMovies.BusinessLogic/Entities/Importers/CastMemberMovieImporter.cs
+++ BlazorWebAppMovies.BusinessLogic/Entities/Importers/CastMemberMovieImporter.cs
@@ -63,6 +63,7 @@ public static class CastMemberMovieImporter
 
                 var dbCastMemberMovie = await context.CastMemberMovies.SingleOrDefaultAsync(
                     x => true
+                    && x.CastMember == castMember
                     // CompositeKeyCodePlaceholder
                 );
 
