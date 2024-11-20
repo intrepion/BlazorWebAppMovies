23c8b1874941dbde71d020099da26cdaaa222f18
Oliver Forral <intrepion@gmail.com>
2024-11-20 15:42:49 -0700

    gawk -i inplace "{ gsub(/\/\/ ManyToOneCodePlaceholder/, \");\n\n\ \ \ \ \ \ \ \ \ \ \ \ // ManyToOneCodePlaceholder\") }; { print }" BlazorWebAppMovies.BusinessLogic/Entities/Importers/CastMemberMovieImporter.cs

--- BlazorWebAppMovies.BusinessLogic/Entities/Importers/CastMemberMovieImporter.cs
+++ BlazorWebAppMovies.BusinessLogic/Entities/Importers/CastMemberMovieImporter.cs
@@ -53,6 +53,8 @@ public static class CastMemberMovieImporter
                 true
                 && x.NormalizedName1 == record.CastMember_NormalizedName1
                 && x.NormalizedName2 == record.CastMember_NormalizedName2
+            );
+
             // ManyToOneCodePlaceholder
 
             if (true
