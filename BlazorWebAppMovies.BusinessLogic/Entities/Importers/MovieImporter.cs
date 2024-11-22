f74cc7cd60a8909b23c76ce7350d09c5493ba597
Oliver Forral <intrepion@gmail.com>
2024-11-21 20:31:30 -0700

    gawk -i inplace "{ gsub(/\/\/ ExistingEntityCodePlaceholder/, \"dbMovie.Title = record.Title;\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ // ExistingEntityCodePlaceholder\") }; { print }" BlazorWebAppMovies.BusinessLogic/Entities/Importers/MovieImporter.cs

--- BlazorWebAppMovies.BusinessLogic/Entities/Importers/MovieImporter.cs
+++ BlazorWebAppMovies.BusinessLogic/Entities/Importers/MovieImporter.cs
@@ -77,6 +77,7 @@ public static class MovieImporter
                 {
                     dbMovie.ApplicationUserUpdatedBy = applicationUserUpdatedBy;
 
+                    dbMovie.Title = record.Title;
                     // ExistingEntityCodePlaceholder
                 }
             }
