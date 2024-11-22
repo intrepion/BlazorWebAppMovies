8fb2a9740fa28c9979053b501c52964decc30cf8
Oliver Forral <intrepion@gmail.com>
2024-11-21 20:26:46 -0700

    gawk -i inplace "{ gsub(/\/\/ ModifyPropertyCodePlaceholder/, \"await Page.GetByLabel(\\\"Name 1:\\\", new() { Exact = true }).FillAsync(\\\"someName1\\\" + someRandomString);\n\ \ \ \ \ \ \ \ // ModifyPropertyCodePlaceholder\") }; { print }" BlazorWebAppMovies.AcceptanceTests/Admin/CastMemberAdminPages.cs

--- BlazorWebAppMovies.AcceptanceTests/Admin/CastMemberAdminPages.cs
+++ BlazorWebAppMovies.AcceptanceTests/Admin/CastMemberAdminPages.cs
@@ -27,6 +27,7 @@ public partial class CastMemberAdminPages : PageTest
         await Page.GetByRole(AriaRole.Link, new() { Name = "Edit" }).ClickAsync();
         await Expect(Page).ToHaveTitleAsync("Cast Member Edit");
 
+        await Page.GetByLabel("Name 1:", new() { Exact = true }).FillAsync("someName1" + someRandomString);
         // ModifyPropertyCodePlaceholder
 
         await Page.GetByRole(AriaRole.Button, new() { Name = "Submit" }).ClickAsync();
