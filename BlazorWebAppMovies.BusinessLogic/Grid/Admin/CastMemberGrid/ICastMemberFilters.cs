0248578e72ec3668a08c96aeb5b08d4d5f843771
Oliver Forral <intrepion@gmail.com>
2024-11-21 20:26:22 -0700

    gawk -i inplace "{ gsub(/EntityNamePlaceholder/, \"CastMember\") }; { print }" BlazorWebAppMovies.BusinessLogic/Grid/Admin/CastMemberGrid/ICastMemberFilters.cs

--- BlazorWebAppMovies.BusinessLogic/Grid/Admin/CastMemberGrid/ICastMemberFilters.cs
+++ BlazorWebAppMovies.BusinessLogic/Grid/Admin/CastMemberGrid/ICastMemberFilters.cs
@@ -1,8 +1,8 @@
-﻿namespace BlazorWebAppMovies.BusinessLogic.Grid.Admin.EntityNamePlaceholderGrid;
+﻿namespace BlazorWebAppMovies.BusinessLogic.Grid.Admin.CastMemberGrid;
 
-public interface IEntityNamePlaceholderFilters
+public interface ICastMemberFilters
 {
-    EntityNamePlaceholderFilterColumns FilterColumn { get; set; }
+    CastMemberFilterColumns FilterColumn { get; set; }
 
     bool Loading { get; set; }
 
@@ -12,5 +12,5 @@ public interface IEntityNamePlaceholderFilters
 
     bool SortAscending { get; set; }
 
-    EntityNamePlaceholderFilterColumns SortColumn { get; set; }
+    CastMemberFilterColumns SortColumn { get; set; }
 }
