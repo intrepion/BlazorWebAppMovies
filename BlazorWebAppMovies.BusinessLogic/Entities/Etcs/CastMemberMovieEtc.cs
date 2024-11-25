0d7bdacdf4fe269ddb62f7c7a2a2c42ae0ab7784
Oliver Forral <intrepion@gmail.com>
2024-11-25 15:40:25 -0700

    gawk -i inplace "{ gsub(/\/\/ EntityConfigurationCodePlaceholder/, \"builder.HasOne(x => x.CastMember)\n\ \ \ \ \ \ \ \ \ \ \ \ .WithMany(x => x.CastMemberMovies)\n\ \ \ \ \ \ \ \ \ \ \ \ .OnDelete(DeleteBehavior.Restrict);\n\ \ \ \ \ \ \ \ // EntityConfigurationCodePlaceholder\") }; { print }" BlazorWebAppMovies.BusinessLogic/Entities/Etcs/CastMemberMovieEtc.cs

--- BlazorWebAppMovies.BusinessLogic/Entities/Etcs/CastMemberMovieEtc.cs
+++ BlazorWebAppMovies.BusinessLogic/Entities/Etcs/CastMemberMovieEtc.cs
@@ -13,6 +13,9 @@ public class CastMemberMovieEtc : IEntityTypeConfiguration<CastMemberMovie>
             .WithMany(x => x.UpdatedCastMemberMovies)
             .OnDelete(DeleteBehavior.Restrict);
 
+        builder.HasOne(x => x.CastMember)
+            .WithMany(x => x.CastMemberMovies)
+            .OnDelete(DeleteBehavior.Restrict);
         // EntityConfigurationCodePlaceholder
     }
 }
