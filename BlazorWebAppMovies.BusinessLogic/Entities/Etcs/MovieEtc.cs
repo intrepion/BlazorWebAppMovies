ef0a6f1e2b2c5156b84e5df987c96cd52f4178a3
Oliver Forral <intrepion@gmail.com>
2024-11-28 15:58:07 -0700

    gawk -i inplace "{ gsub(/EntityNamePlaceholder/, \"Movie\") }; { print }" BlazorWebAppMovies.BusinessLogic/Entities/Etcs/MovieEtc.cs

--- BlazorWebAppMovies.BusinessLogic/Entities/Etcs/MovieEtc.cs
+++ BlazorWebAppMovies.BusinessLogic/Entities/Etcs/MovieEtc.cs
@@ -3,9 +3,9 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace ApplicationNamePlaceholder.BusinessLogic.Entities.Configuration;
 
-public class EntityNamePlaceholderEtc : IEntityTypeConfiguration<EntityNamePlaceholder>
+public class MovieEtc : IEntityTypeConfiguration<Movie>
 {
-    public void Configure(EntityTypeBuilder<EntityNamePlaceholder> builder)
+    public void Configure(EntityTypeBuilder<Movie> builder)
     {
         builder.HasOne(x => x.ApplicationUserUpdatedBy)
             .WithMany(x => x.UpdatedTableNamePlaceholder)
