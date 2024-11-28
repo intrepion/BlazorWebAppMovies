793259640a60109802a534ee6a2e3e4d10bde814
Oliver Forral <intrepion@gmail.com>
2024-11-27 20:16:20 -0700

    gawk -i inplace "{ gsub(/\/\/ ActualPropertyPlaceholder/, \"[Required]\n\ \ \ \ // ActualPropertyPlaceholder\") }; { print }" BlazorWebAppMovies.BusinessLogic/Entities/CastMember.cs

--- BlazorWebAppMovies.BusinessLogic/Entities/CastMember.cs
+++ BlazorWebAppMovies.BusinessLogic/Entities/CastMember.cs
@@ -16,5 +16,6 @@ public class CastMember
     public string NormalizedName1 { get; set; } = string.Empty;
     [Required]
     public string Name2 { get; set; } = string.Empty;
+    [Required]
     // ActualPropertyPlaceholder
 }
