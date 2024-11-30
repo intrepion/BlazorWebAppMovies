3f956130e0b5be46bdf46aae227d9800797d2c28
Oliver Forral <intrepion@gmail.com>
2024-11-30 16:45:15 -0700

    dotnet ef --project BlazorWebAppMovies.BusinessLogic --startup-project BlazorWebAppMovies migrations add Initial

---
+++ BlazorWebAppMovies.BusinessLogic/Grid/Admin/ApplicationUserGrid/ApplicationUserFilterColumns.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorWebAppMovies.BusinessLogic.Grid.Admin.ApplicationUserGrid;
+
+public enum ApplicationUserFilterColumns
+{
+    Id,
+    Email,
+    PhoneNumber,
+    UserName,
+}
---
+++ BlazorWebAppMovies.BusinessLogic/Grid/Admin/CastMemberMovieGrid/ICastMemberMovieFilters.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorWebAppMovies.BusinessLogic.Grid.Admin.CastMemberMovieGrid;
+
+public interface ICastMemberMovieFilters
+{
+    CastMemberMovieFilterColumns FilterColumn { get; set; }
+
+    bool Loading { get; set; }
+
+    string? FilterText { get; set; }
+
+    IPageHelper PageHelper { get; set; }
+
+    bool SortAscending { get; set; }
+
+    CastMemberMovieFilterColumns SortColumn { get; set; }
+}
