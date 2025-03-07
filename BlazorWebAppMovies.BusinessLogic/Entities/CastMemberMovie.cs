4099411c3259dfcec0ab5ee2f519d82d7cd5eb95
Oliver Forral <intrepion@gmail.com>
2025-03-07 00:14:32 -0700

    dotnet ef --project BlazorWebAppMovies.BusinessLogic --startup-project BlazorWebAppMovies migrations add Initial

---
+++ BlazorWebAppMovies.BusinessLogic/Entities/CastMemberMovie.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorWebAppMovies.BusinessLogic.Entities;
+
+public class CastMemberMovie
+{
+    public ApplicationUser? ApplicationUserUpdatedBy { get; set; }
+    public Guid Id { get; set; }
+    public DateTime UpdateDateTime { get; set; }
+
+    public CastMember? CastMember { get; set; }
+    public Movie? Movie { get; set; }
+    // ActualPropertyPlaceholder
+}
