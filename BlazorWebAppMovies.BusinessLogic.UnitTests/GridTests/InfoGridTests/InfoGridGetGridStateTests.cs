f8aa2dac16bab0850d811ef2f4774135cc5a8d2b
Oliver Forral <intrepion@gmail.com>
2025-03-15 21:58:13 -0600

    Assert multiple.

--- BlazorWebAppMovies.BusinessLogic.UnitTests/GridTests/InfoGridTests/InfoGridGetGridStateTests.cs
+++ BlazorWebAppMovies.BusinessLogic.UnitTests/GridTests/InfoGridTests/InfoGridGetGridStateTests.cs
@@ -48,10 +48,14 @@ public class InfoGridGetGridStateTests
 
         var actual = _infoGridApplicationUser._infoGridState;
 
-        Assert.That(actual._columns, Is.EqualTo(expected._columns));
-        Assert.That(actual._page, Is.EqualTo(expected._page));
-        Assert.That(actual._rowsPerPage, Is.EqualTo(expected._rowsPerPage));
-        Assert.That(actual._sorts, Is.EqualTo(expected._sorts));
-        Assert.That(actual._totalRows, Is.EqualTo(expected._totalRows));
+        Assert.Multiple(() =>
+        {
+            Assert.That(actual._columns, Is.EqualTo(expected._columns));
+            Assert.That(actual._page, Is.EqualTo(expected._page));
+            Assert.That(actual._rowsPerPage, Is.EqualTo(expected._rowsPerPage));
+            Assert.That(actual._sorts, Is.EqualTo(expected._sorts));
+            Assert.That(actual._totalRows, Is.EqualTo(expected._totalRows));
+        });
+
     }
 }
