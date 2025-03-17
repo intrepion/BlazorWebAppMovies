35d53ea5059324041f18da3f6e583e8752fbb327
Oliver Forral <intrepion@gmail.com>
2025-03-17 13:09:52 -0600

    Split up test files.

--- BlazorWebAppMovies.BusinessLogic.UnitTests/GridTests/InfoGridTests/InfoGridGetGridStateTests/InfoGridGetGridStateNextPageTests.cs
+++ BlazorWebAppMovies.BusinessLogic.UnitTests/GridTests/InfoGridTests/InfoGridGetGridStateTests/InfoGridGetGridStateNextPageTests.cs
@@ -1,9 +1,9 @@
 ﻿using BlazorWebAppMovies.BusinessLogic.Entities;
 using BlazorWebAppMovies.BusinessLogic.Grid;
 
-namespace BlazorWebAppMovies.BusinessLogic.UnitTests;
+namespace BlazorWebAppMovies.BusinessLogic.UnitTests.InfoGridTests.InfoGridGetGridStateTests;
 
-public class InfoGridGetGridStateTests
+public class InfoGridGetGridStateNextPageTests
 {
     private List<ApplicationUser> _listApplicationUser;
     private InfoGrid<ApplicationUser> _infoGridApplicationUser;
@@ -242,34 +242,6 @@ public class InfoGridGetGridStateTests
         _infoGridApplicationUser = new InfoGrid<ApplicationUser>();
 
         _infoGridApplicationUser.SetInitialInfo(_listApplicationUser);
-    }
-
-    [Test]
-    public void DefaultState_WhenNothing()
-    {
-        var expected = new InfoGridState
-        {
-            _columns = 3,
-            _filters = [],
-            _page = 1,
-            _rowsPerPage = 10,
-            _sorts = [],
-            _totalPages = 3,
-            _totalRows = 28,
-        };
-
-        var actual = _infoGridApplicationUser._infoGridState;
-
-        Assert.Multiple(() =>
-        {
-            Assert.That(actual._columns, Is.EqualTo(expected._columns), $"Columns is {actual._columns}, but should be {expected._columns}");
-            Assert.That(actual._filters, Is.EqualTo(expected._filters), $"Filters is {actual._filters}, but should be {expected._filters}");
-            Assert.That(actual._page, Is.EqualTo(expected._page), $"Page is {actual._page}, but should be {expected._page}");
-            Assert.That(actual._rowsPerPage, Is.EqualTo(expected._rowsPerPage), $"Rows Per Page is {actual._rowsPerPage}, but should be {expected._rowsPerPage}");
-            Assert.That(actual._sorts, Is.EqualTo(expected._sorts), $"Sorts is {actual._sorts}, but should be {expected._sorts}");
-            Assert.That(actual._totalPages, Is.EqualTo(expected._totalPages), $"Total Pages is {actual._totalPages}, but should be {expected._totalPages}");
-            Assert.That(actual._totalRows, Is.EqualTo(expected._totalRows), $"Total Rows is {actual._totalRows}, but should be {expected._totalRows}");
-        });
     }
 
     [Test]
