410e6f2e1aeaea4944028a1d4c86c22b101b4bae
Oliver Forral <intrepion@gmail.com>
2025-04-28 18:57:25 -0600

    dotnet ef --project BlazorWebAppMovies.BusinessLogic --startup-project BlazorWebAppMovies migrations add Initial

---
+++ BlazorWebAppMovies.BusinessLogic/Entities/Etcs/MovieEtc.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BlazorWebAppMovies.BusinessLogic.Entities.Configuration;
+
+public class MovieEtc : IEntityTypeConfiguration<Movie>
+{
+    public void Configure(EntityTypeBuilder<Movie> builder)
+    {
+        builder.HasOne(x => x.ApplicationUserUpdatedBy)
+            .WithMany(x => x.UpdatedMovies)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        // EntityConfigurationCodePlaceholder
+    }
+}
