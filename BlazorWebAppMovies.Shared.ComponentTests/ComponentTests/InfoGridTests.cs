2e11a169586618c0b17e21f4379e00668f0d84b5
Oliver Forral <intrepion@gmail.com>
2025-04-29 23:44:28 -0600

    dotnet format

--- BlazorWebAppMovies.Shared.ComponentTests/ComponentTests/InfoGridTests.cs
+++ BlazorWebAppMovies.Shared.ComponentTests/ComponentTests/InfoGridTests.cs
@@ -1,4 +1,4 @@
-using BlazorWebAppMovies.Shared.Grid;
+﻿using BlazorWebAppMovies.Shared.Grid;
 using Bunit;
 using NUnit.Framework;
 
@@ -6,11 +6,11 @@ namespace BlazorWebAppMovies.Shared.UnitTests.InfoGridTests;
 
 public class InfoGridTests : BunitContext
 {
-  [Test]
-  public void InfoGridComponentRendersCorrectly()
-  {
-    var cut = Render<InfoGrid>();
+    [Test]
+    public void InfoGridComponentRendersCorrectly()
+    {
+        var cut = Render<InfoGrid>();
 
-    cut.MarkupMatches("<div><p>No information found.</p></div>");
-  }
+        cut.MarkupMatches("<div><p>No information found.</p></div>");
+    }
 }
