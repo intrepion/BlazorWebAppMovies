fd7d86ff7ffe66ae81a265ebe6380301824b190e
Oliver Forral <intrepion@gmail.com>
2025-05-05 23:19:20 -0600

    dotnet format

--- BlazorWebAppMovies.BusinessLogic/Entities/Importers/CastMemberImporter.cs
+++ BlazorWebAppMovies.BusinessLogic/Entities/Importers/CastMemberImporter.cs
@@ -51,7 +51,7 @@ public static class CastMemberImporter
             // ManyToOneCodePlaceholder
 
             if (true
-                // NullCheckCodePlaceholder
+            // NullCheckCodePlaceholder
             )
             {
                 var castMember = new CastMember
