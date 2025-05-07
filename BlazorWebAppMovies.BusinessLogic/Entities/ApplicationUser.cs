f66e0cb9b50ec7a0dc4e1a95ea6bd5b055a1d5c1
Oliver Forral <intrepion@gmail.com>
2025-05-06 21:55:35 -0600

    dotnet ef --project BlazorWebAppMovies.BusinessLogic --startup-project BlazorWebAppMovies migrations add Initial

---
+++ BlazorWebAppMovies.BusinessLogic/Entities/ApplicationUser.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorWebAppMovies.BusinessLogic.Entities;
+
+public class ApplicationUser : IdentityUser<Guid>
+{
+    public ICollection<ApplicationUserRole> ApplicationUserRoles { get; set; } = [];
+    public ApplicationUser? ApplicationUserUpdatedBy { get; set; }
+    public DateTime UpdateDateTime { get; set; }
+    public ICollection<ApplicationRole> UpdatedApplicationRoles { get; set; } = [];
+    public ICollection<ApplicationRoleClaim> UpdatedApplicationRoleClaims { get; set; } = [];
+    public ICollection<ApplicationUser> UpdatedApplicationUsers { get; set; } = [];
+    public ICollection<ApplicationUserClaim> UpdatedApplicationUserClaims { get; set; } = [];
+    public ICollection<ApplicationUserLogin> UpdatedApplicationUserLogins { get; set; } = [];
+    public ICollection<ApplicationUserRole> UpdatedApplicationUserRoles { get; set; } = [];
+    public ICollection<ApplicationUserToken> UpdatedApplicationUserTokens { get; set; } = [];
+
+    public ICollection<CastMember> UpdatedCastMembers { get; set; } = [];
+    public ICollection<CastMemberMovie> UpdatedCastMemberMovies { get; set; } = [];
+    public ICollection<Movie> UpdatedMovies { get; set; } = [];
+    // ActualPropertyPlaceholder
+}
